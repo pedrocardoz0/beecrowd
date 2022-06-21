d1dad17226730b32b851e4e0dc39ebf1c4a37b6b
Pedro Cardozo <p-cardozo@hotmail.com>
2022-06-21 08:49:01 -0300

    submit: p_1003

---
+++ p_1003/src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    let mut _a = String::new();
+    let mut _b = String::new();
+
+    io::stdin().read_line(&mut _a).unwrap();
+    io::stdin().read_line(&mut _b).unwrap();
+
+    let a: i32 = _a.trim().parse().unwrap();
+    let b: i32 = _b.trim().parse().unwrap();
+
+    let soma: i32 = a + b;
+
+    println!("SOMA = {}", soma);
+}
